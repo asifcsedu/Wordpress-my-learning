dcc5ba4ef56d398124cc61de95bb59a8dc83be99
asifcsedu <asifcsedu@gmail.com>
2024-10-18 12:35:09 +0600

    Update Oct 18 2024

--- page-past-events.php
+++ page-past-events.php
@@ -6,17 +6,36 @@
         style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
       </div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"> All Events </h1>
+        <h1 class="page-banner__title"> Past Events </h1>
         <div class="page-banner__intro">
-          <p>See what is going on at our University</p>
+          <p>A Recap of Our Past Events</p>
         </div>
       </div>
 </div>
 
 <div class="container container--narrow page-section">
   <?php
-    while(have_posts()){
-        the_post();
+    
+    $today = date('Ymd');
+          $pastEvents = new WP_Query(array(
+            'paged' => get_query_var( 'paged' ),
+            'posts_per_page' => 1,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            )
+          ));
+
+    while($pastEvents->have_posts()){
+        $pastEvents->the_post();
   ?>
       <div class="event-summary">
       <a class="event-summary__date t-center" href="#">
@@ -25,7 +44,7 @@
                   echo $eventDate->format('M')
                 ?></span>
                 <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
-              </a>
+              </a> 
         <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink( ); ?>"><?php the_title(); ?></a></h5>
             <p> <?php echo wp_trim_words(get_the_content( ), 18) ?> <a href="<?php the_permalink( ); ?>" class="nu gray">Learn more</a></p>
@@ -33,7 +52,9 @@
       </div>
   <?php
     }
-    echo paginate_links();
+    echo paginate_links(array(
+        'total' => 4
+    ));
   ?>
 
 </div>

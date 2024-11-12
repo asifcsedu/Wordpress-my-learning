82420267a76fad623ba9b3dc19d19531dcc6608c
asifcsedu <asifcsedu@gmail.com>
2024-11-13 01:37:22 +0600

    Adding post type Program

--- single-event.php
+++ single-event.php
@@ -27,6 +27,22 @@
             <div class="generic-content">
                 <?php the_content(); ?>
             </div>
+
+            <?php
+                $relatedPrograms = get_field('related_programs');
+                //print_r($relatedPrograms);
+                
+                if($relatedPrograms){
+                        echo '<hr class="section-break">';
+                        echo '<h2 class="headline headline--small">Related Program(s) </h2>';
+                        echo '<ul class=" min-list">';
+                        foreach($relatedPrograms as $program){ ?>
+                            <li><a href="<?php echo get_the_permalink($program)?>"><?php echo get_the_title($program);?></a></li>
+
+                    <?php }
+                    echo '</ul>';
+                }
+            ?>
         </div>
     <?php 
     }

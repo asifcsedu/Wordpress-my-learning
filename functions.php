35ee12d0e8dedb08edc1c4a8fdbeeed3062cabdb
asifcsedu <asifcsedu@gmail.com>
2024-11-16 04:50:04 +0600

    Adding post type Professor and image handling

--- functions.php
+++ functions.php
@@ -13,6 +13,9 @@ function university_features(){
   //register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
   //register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400,260,true);
+  add_image_size('professorPortrait', 480,650,true);
 }
 
 function university_adjust_queries($query){
